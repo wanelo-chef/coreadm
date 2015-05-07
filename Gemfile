28acf7c2ef29b863f4b02e002e0d3597e1f72d5e
Eric Saxby <eric@wanelo.com>
2015-05-07 14:24:12 -0700

    Add test harness

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'berkshelf'
+gem 'emeril'
+gem 'foodcritic'
+gem 'kitchen-vagrant'
+gem 'test-kitchen'
+gem 'rubocop'
+gem 'rspec'
--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             'coreadm'
-maintainer       'Wanelo, Inc.'
-maintainer_email 'bixu@wanelo.com'
-license          'Apache 2.0'
-description      'Configures coreadm'
+name 'coreadm'
+maintainer 'Wanelo, Inc.'
+maintainer_email 'dev@wanelo.com'
+license 'MIT'
+description 'Configures coreadm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version '0.1.1'
